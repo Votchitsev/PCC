1f4bb94b447efaddfdc73b24dc2e95d32a02cc41
Dmitry <dmitry.votchitsev@gmail.com>
2023-12-24 18:53:51 +0200

    [+] add CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+
 from db.database import database
 from modules.router import router as api_router
 
@@ -13,6 +15,21 @@ app = FastAPI(
 app.mount('/static', StaticFiles(directory='../frontend/dist'))
 
 frontend = Jinja2Templates(directory="templates")
+
+origins = [
+    'http://localhost',
+    'http://localhost:8000',
+    'http://45.140.146.226:8000/',
+    'https://45.140.146.226/',
+]
+
+app.middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
